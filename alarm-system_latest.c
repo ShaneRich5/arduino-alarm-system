a36495cf9960e52dfa19cf285e36eec5555cae49
shane <shane.richards121@gmail.com>
2016-04-11 12:18:37 -0500

    go hard and done

--- alarm-system_latest.c
+++ alarm-system_latest.c
@@ -1,10 +1,8 @@
 #define SENSOR1 A0
+#define SENSOR2 A1
 
 // Declarations
 int pin_switch = 3,
-    // analog pins
-    pin_hes_1 = 0,
-    pin_hes_2 = 1,
     // digital pins
     pin_speaker = 11,
     pin_led = 2;
@@ -18,37 +16,31 @@ long hes_value_1,
 void setup() {
   Serial.begin(9600);
   pinMode(pin_speaker , OUTPUT);
-  pinMode(pin_led, OUTPUT);
-
-
-  // pins for hes
-  pinMode(pin_hes_1, INPUT);
-  pinMode(pin_hes_2, INPUT);
+  pinMode(pin_led, OUTPUT);  
 
   // output
   pinMode(pin_switch, INPUT);
 }
 
 void loop() {
+  // read input 
   controller = digitalRead(pin_switch);
-//  Serial.println(controller);
+  
+  //  Serial.println(controller);
   if (controller == HIGH){
     val_1 = analogRead(SENSOR1);
-      if(val_1 >= 500 && val_1 <= 530){
-  //    long val_2 = analogRead(pin_hes_2); //to comment
-      //int val = digitalRead(pin_hes_1);
-      Serial.println(val_1);
-      delay(3000);
-      analogWrite(pin_speaker, 3);
-      digitalWrite(pin_led, HIGH);
-      //  Serial.println(pinHes_1);
-      //  current_time = millis();
-      //  if (pinSwitch == HIGH) {
-      //    while (millis() <= current_time + 60000) {
-  
-      //    }
-      //  }
-    
+      if ((val_1 >= 500 && val_1 <= 530) && (val_2 >= 500 && val_2 <= 530)) {
+        Serial.println(val_1);
+        delay(3000);
+        analogWrite(pin_speaker, 3);
+        digitalWrite(pin_led, HIGH);
+      
+      // Serial.println(pinHes_1);
+      // current_time = millis();
+      // if (pinSwitch == HIGH) {
+          // while (millis() <= current_time + 60000) {
+          // }
+      // }
     }else{}
   }else{
     analogWrite(pin_speaker, 0);
